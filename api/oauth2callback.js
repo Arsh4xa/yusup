914567e607c86f8ca36f5c8a00e42084cf47601e
Arsh4xa <ov792000@gmail.com>
2025-01-22 22:48:58 +0700

    Create oauth2callback.js

---
+++ api/oauth2callback.js
@@ -0,0 +1,20 @@
+const { google } = require("googleapis");
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+module.exports = async (req, res) => {
+  const code = req.query.code;
+
+  try {
+    const { tokens } = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+    res.status(200).send("Authentication successful!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Authentication failed.");
+  }
+};
